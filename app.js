20bce5a3a5c5ac10070c07c238c8f2be8f668301
Gnanigit <gnani4412@gmail.com>
2024-01-20 00:45:42 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,29 +1,86 @@
-const express = require('express')
-const app = express()
+const express = require('express') // importing the module
+const app = express() // creating a application
 const { Todo } = require('./models')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser') // to do read request body to look for title and duedate so inorder to read we need body-parser to accept and parse the json request body
 app.use(bodyParser.json())
 
-app.post('/todos', async (req, res) => {
-  console.log('Created a todo', req.body)
-  try {
-    const todo = await Todo.addTodo({ title: req.body.title, dueDate: req.body.dueDate, completed: false })
-    return res.json(todo)
-  } catch (err) {
-    console.log(err)
-    return res.status(422).json(err)
+
+app.set("view engine","ejs");
+
+app.get("/", async (request,response)=>{
+  //  console.log("hello");
+  //  response.render('index');
+  const allTodos = await Todo.getTodos();
+  if(request.accepts('html')){
+    response.render('index',{allTodos});
+  }
+  else{
+    response.json({allTodos})
   }
 })
 
-app.put('/todos/:id/marksAsCompleted', async (req, res) => {
-  const todo = await Todo.findByPk(req.params.id)
+app.get('/todos', async (request, response) => {
+  console.log('todo list')
+ 
+  try {
+    const todos = await Todo.findAll();
+    const formattedTodos = todos.map(todo => ({
+      id: todo.id,
+      name: todo.title, 
+      dueDate: todo.dueDate,
+      completed: todo.completed
+    }));
+
+    return response.json(formattedTodos);
+  } catch (error) {
+    console.error('Error fetching todos:', error);
+    return response.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+app.post('/todos', async (request, response) => {
+  console.log('creating todo list', request.body)
+
+  try {
+    const todo = await Todo.addTodo({ title: request.body.title, dueDate: request.body.dueDate, completed: false })
+    return response.json(todo)
+  } catch (error) {
+    console.log(error)
+    return response.status(422).json(error)
+  }
+})
+
+app.put('/todos/:id/markAsCompleted', async (request, response) => {
+  console.log('we have to upadate todo with ID', request.params.id)
+  const todo = await Todo.findByPk(request.params.id)
   try {
     const updatedTodo = await todo.markAsCompleted()
-    return res.json(updatedTodo)
-  } catch (err) {
-    console.log(err)
-    return res.status(422).json(err)
+    return response.json(updatedTodo)
+  } catch (error) {
+    console.log(error)
+    return response.status(422).json(error)
   }
 })
 
-module.exports = app
+app.delete('/todos/:id', async (request, response) => {
+  console.log('delete todo by ID', request.params.id)
+    const todoId = request.params.id;
+    console.log('delete todo by ID', todoId);
+  
+    try {
+      const todo = await Todo.findByPk(todoId);
+      if (!todo) {
+        return response.status(404).json({ success: false, message: 'Todo not found' });
+      }
+  
+      await todo.destroy();
+  
+      return response.json({ success: true, message: 'Todo deleted successfully' });
+    } catch (error) {
+      console.error('Error deleting todo:', error);
+      return response.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+  });
+
+
+module.exports=app;
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-import { listen } from './app'
-listen(3000)
+const app=require('./app');
+app.listen(3000,()=>{
+    console.log("started");
+})
